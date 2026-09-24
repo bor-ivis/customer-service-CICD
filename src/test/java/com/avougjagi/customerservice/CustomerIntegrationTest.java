eaf9711a9f7cf499c21e9f42a4c1a56bc21cbd2d
Boris Ivis <boris.ivis@hotmail.com>
2026-09-24 05:09:31 +0200

    force fail integration test

--- src/test/java/com/avougjagi/customerservice/CustomerIntegrationTest.java
+++ src/test/java/com/avougjagi/customerservice/CustomerIntegrationTest.java
@@ -32,7 +32,7 @@ class CustomerIntegrationTest {
         mockMvc.perform(post("/api/customers")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(validCustomerJson))
-                .andExpect(status().isCreated());
+                .andExpect(status().isBadRequest());
     }
 
     //Returnerar 400 om namn är tomt
--- src/test/java/com/avougjagi/customerservice/CustomerServiceTest.java
+++ src/test/java/com/avougjagi/customerservice/CustomerServiceTest.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-//test for CI
+
 @ExtendWith(MockitoExtension.class)
 class CustomerServiceTest {
 
